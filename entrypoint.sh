dfb8aaed0dca2a4cc39bb80e6c3837a7f6d83b94
Alex R <alex@ironrobin.net>
2025-08-27 09:36:24 -0500

    build telepathy-qt

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ gpg --homedir /root/.gnupg --list-keys
 
 sudo pacman -S base-devel --noconfirm --needed
 
-for i in "accountsservice-ubuntu" "qt5-pim-git" "telepathy-farstream" ; do
+for i in "accountsservice-ubuntu" "qt5-pim-git" "telepathy-farstream" "telepathy-qt" ; do
 	status=13
 	git submodule update --init $i
 	cd $i
