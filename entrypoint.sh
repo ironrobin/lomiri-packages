93defb42c6ec1e30bdb794be7c4939b7353525bd
Alex R <alex@ironrobin.net>
2025-08-26 19:33:21 -0500

    package qt5-pim-git

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ gpg --homedir /root/.gnupg --list-keys
 
 sudo pacman -S base-devel --noconfirm --needed
 
-for i in "accountsservice-ubuntu" "qt5-pim-git ; do
+for i in "accountsservice-ubuntu" "qt5-pim-git" ; do
 	status=13
 	git submodule update --init $i
 	cd $i
