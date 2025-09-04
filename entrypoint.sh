7e55883a99eeafc7769c2b555ec95d76dde2b98e
Alex R <alex@ironrobin.net>
2025-09-04 15:19:58 -0500

    build layer 5

--- entrypoint.sh
+++ entrypoint.sh
@@ -79,7 +79,7 @@ pkgs=(
   gmenuharness-git
   lomiri-thumbnailer
   # # Layer 5
-  # lomiri-url-dispatcher-git
+  lomiri-url-dispatcher-git
   # # Layer 6
   # libayatana-common-git
   # lomiri-indicator-network-git
