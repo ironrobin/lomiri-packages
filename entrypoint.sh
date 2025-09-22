ffc879e90e9bf658721f9dcf8b73d51d5c4b4450
Alex R <alex@ironrobin.net>
2025-09-22 14:41:15 -0500

    build layer 5

--- entrypoint.sh
+++ entrypoint.sh
@@ -84,18 +84,18 @@ pkgs=(
   # Layer 5
   lomiri-url-dispatcher-git
   # Layer 6
-  libayatana-common-git
-  lomiri-indicator-network
-  lomiri-telephony-service-git
-  lomiri-address-book-service-git
-  lomiri-content-hub-git
-  lomiri-system-settings
-  qtmir-git
-  # Layer 7
-  ayatana-indicator-datetime-git
-  lomiri
-  # Layer 8
-  lomiri-session
+  # libayatana-common-git
+  # lomiri-indicator-network
+  # lomiri-telephony-service-git
+  # lomiri-address-book-service-git
+  # lomiri-content-hub-git
+  # lomiri-system-settings
+  # qtmir-git
+  # # Layer 7
+  # ayatana-indicator-datetime-git
+  # lomiri
+  # # Layer 8
+  # lomiri-session
 )
 
 for i in "${pkgs[@]}" ; do
