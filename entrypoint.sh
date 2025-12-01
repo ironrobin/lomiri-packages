dbb40344681926420ab7abc6ec7e99d6d60e2816
Alex R <alex@ironrobin.net>
2025-12-01 16:43:35 +0000

    debug build self-hosted pacman repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -109,49 +109,68 @@ pkgs=(
   lomiri-session
 )
 
-for i in "${pkgs[@]}" ; do
-  status=13
-  git submodule update --init $i
-  cd $i
-  REPONAME="$repo_owner-lomiri"
-  PKGNAME=$(basename $i)
+for i in "humanity-icon-theme" ; do
+	status=13
+	git submodule update --init $i
+	cd $i
 
-  # Extract version from PKGBUILD
-  PKGVERLINE=$(grep -E '^pkgver=' PKGBUILD)
-  PKGVER="${PKGVERLINE#*=}"
-  PKGRELLINE=$(grep -E '^pkgrel=' PKGBUILD)
-  PKGREL="${PKGRELLINE#*=}"
-  VERSION="${PKGVER}-${PKGREL}"
+	for i in $(sudo -u builduser makepkg --packagelist); do
+		package=$(basename $i)
+		# wget https://github.com/$repo_owner/$repo_name/releases/download/packages/$package \
+		# 	&& echo "Warning: $package already built, did you forget to bump the pkgver and/or pkgrel? It will not be rebuilt."
+	done
+	sudo -u builduser bash -c 'export MAKEFLAGS=-j$(nproc) && makepkg --sign -s --noconfirm'||status=$?
 
-  if pacman -Si $PKGNAME &> /dev/null; then
-    REPO_VERSION=$(pacman -Si $PKGNAME | grep Version | awk '{print $3}')
-    echo "REPO_VERSION: $REPO_VERSION"
-    echo "VERSION: $VERSION"
-    echo "PKGNAME: $PKGNAME"
-    if (( $(vercmp "$VERSION" "$REPO_VERSION") < 0 )); then
-      echo "Package $PKGNAME of version $VERSION is older than the version ($REPO_VERSION) in the $REPONAME repo. Not building."
-      sudo pacman -Swdd $PKGNAME --noconfirm --cachedir ./
-    elif [ "$REPO_VERSION" == "$VERSION" ]; then
-      echo "Package $PKGNAME of version $VERSION already exists in the $REPONAME repo. Not building."
-      sudo pacman -Swdd $PKGNAME --noconfirm --cachedir ./
-    else
-      echo "Package $PKGNAME exists but with a different version ($REPO_VERSION) in the $REPONAME repo. Building new version $VERSION."
-      sudo -u builduser bash -c 'export MAKEFLAGS=-j$(nproc) && makepkg --sign -s --noconfirm'||status=$?
-    fi
-  else
-     echo "Package $PKGNAME does not exist in the $REPONAME repo. Adding it."
-     sudo -u builduser bash -c 'export MAKEFLAGS=-j$(nproc) && makepkg --sign -s --noconfirm'||status=$?
-  fi
-	if [ $status != 13 ]; then
-		exit 1
-	fi
+	# Package already built is fine.
+	# if [ $status != 13 ]; then
+	# 	exit 1
+	# fi
 	cd ..
 done
 
-cp */*.pkg.tar.* ./
-gpg --list-keys
-repo-add --sign ./$repo_owner-lomiri.db.tar.gz ./*.pkg.tar.xz
+# for i in "${pkgs[@]}" ; do
+#   status=13
+#   git submodule update --init $i
+#   cd $i
+#   REPONAME="$repo_owner-lomiri"
+#   PKGNAME=$(basename $i)
 
-for i in *.db *.files; do
-cp --remove-destination $(readlink $i) $i
-done
+#   # Extract version from PKGBUILD
+#   PKGVERLINE=$(grep -E '^pkgver=' PKGBUILD)
+#   PKGVER="${PKGVERLINE#*=}"
+#   PKGRELLINE=$(grep -E '^pkgrel=' PKGBUILD)
+#   PKGREL="${PKGRELLINE#*=}"
+#   VERSION="${PKGVER}-${PKGREL}"
+
+#   if pacman -Si $PKGNAME &> /dev/null; then
+#     REPO_VERSION=$(pacman -Si $PKGNAME | grep Version | awk '{print $3}')
+#     echo "REPO_VERSION: $REPO_VERSION"
+#     echo "VERSION: $VERSION"
+#     echo "PKGNAME: $PKGNAME"
+#     if (( $(vercmp "$VERSION" "$REPO_VERSION") < 0 )); then
+#       echo "Package $PKGNAME of version $VERSION is older than the version ($REPO_VERSION) in the $REPONAME repo. Not building."
+#       sudo pacman -Swdd $PKGNAME --noconfirm --cachedir ./
+#     elif [ "$REPO_VERSION" == "$VERSION" ]; then
+#       echo "Package $PKGNAME of version $VERSION already exists in the $REPONAME repo. Not building."
+#       sudo pacman -Swdd $PKGNAME --noconfirm --cachedir ./
+#     else
+#       echo "Package $PKGNAME exists but with a different version ($REPO_VERSION) in the $REPONAME repo. Building new version $VERSION."
+#       sudo -u builduser bash -c 'export MAKEFLAGS=-j$(nproc) && makepkg --sign -s --noconfirm'||status=$?
+#     fi
+#   else
+#      echo "Package $PKGNAME does not exist in the $REPONAME repo. Adding it."
+#      sudo -u builduser bash -c 'export MAKEFLAGS=-j$(nproc) && makepkg --sign -s --noconfirm'||status=$?
+#   fi
+# 	if [ $status != 13 ]; then
+# 		exit 1
+# 	fi
+# 	cd ..
+# done
+
+# cp */*.pkg.tar.* ./
+# gpg --list-keys
+# repo-add --sign ./$repo_owner-lomiri.db.tar.gz ./*.pkg.tar.xz
+
+# for i in *.db *.files; do
+# cp --remove-destination $(readlink $i) $i
+# done
